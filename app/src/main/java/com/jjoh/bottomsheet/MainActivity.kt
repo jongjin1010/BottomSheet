8fe8a31c90d1d1088691f5a72947ce570cb507a2
오종진 <jongjin024@gmail.com>
2022-04-19 21:05:54 +0900

    🎨 :: BaseActivity 적용

--- app/src/main/java/com/jjoh/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/jjoh/bottomsheet/MainActivity.kt
@@ -2,10 +2,11 @@ package com.jjoh.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jjoh.bottomsheet.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+        binding.activity = this
     }
+
 }
