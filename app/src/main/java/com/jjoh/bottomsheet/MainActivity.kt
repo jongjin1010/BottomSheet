5129da58009e4b0d7b729f4197662e790053ce45
오종진 <jongjin024@gmail.com>
2022-04-20 17:50:52 +0900

    ✨ :: BottomSheet 구현

--- app/src/main/java/com/jjoh/bottomsheet/MainActivity.kt
+++ app/src/main/java/com/jjoh/bottomsheet/MainActivity.kt
@@ -2,11 +2,21 @@ package com.jjoh.bottomsheet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.jjoh.bottomsheet.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun init() {
         binding.activity = this
+
+        setSheet()
     }
 
+    private fun setSheet() {
+        val sheet = binding.sheet
+
+        BottomSheetBehavior.from(sheet).apply {
+            this.state = BottomSheetBehavior.STATE_DRAGGING
+        }
+    }
 }
